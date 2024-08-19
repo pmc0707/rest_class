acf30a4756f3a027d6a9042694e6c13c40474870
pmc0707 <pranshugoodboy@gmail.com>
2024-08-19 10:33:08 +0530

    CURD operation and index route

---
+++ index.js
@@ -0,0 +1,35 @@
+ 
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const path = require('path');
+
+app.use(express.urlencoded({extended:true}))
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+let posts = [
+    {
+        username : "apnacollege",
+        content : "hard work improve"
+    },
+    {
+        username: "physics wallah",
+        content: "12th foundation"
+    }
+];
+app.get("/posts",(req,res)=>
+{
+    res.render("index.ejs",{posts});
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+})
