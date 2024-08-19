8e600c1c62690a7a4562532828fccd1e692a88e2
pmc0707 <pranshugoodboy@gmail.com>
2024-08-19 11:01:18 +0530

    sending the data to the sever and save it on the server

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ app.get("/posts",(req,res)=>
 app.get("/posts/new",(req,res)=>{
     res.render("new.ejs");
 })
+app.post("/posts", (req, res)=>{
+    let { username, content} =req.body;
+    posts.push( {username, content});
+    res.send("req has been sent");
+
+})
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
