335574c4bbad38592fb641f287feebc055954555
pmc0707 <pranshugoodboy@gmail.com>
2024-08-19 11:08:09 +0530

    redirect all thr pages using redirect method

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.get("/posts/new",(req,res)=>{
 app.post("/posts", (req, res)=>{
     let { username, content} =req.body;
     posts.push( {username, content});
-    res.send("req has been sent");
-
+    // res.send("req has been sent");
+    res.redirect("posts");
 })
 
 app.listen(port, ()=>{
